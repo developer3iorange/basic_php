56a66e8a001ee644d787691eb70fb8096c09eb0d
Viswa <developer3@iorange.in>
2026-01-22 10:22:35 +0530

    Update header and navigation

--- includes/nav.php
+++ includes/nav.php
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 <nav>
     <a href="<?= BASE_URL ?>">Home</a> |
     <a href="<?= BASE_URL ?>about.php">About</a> |
     <a href="<?= BASE_URL ?>contact.php">Contact</a>
 </nav>
 <hr>
-=======
->>>>>>> 99ebe5d7f8533ee35ebaaf870659356c2f3efb13
